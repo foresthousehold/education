1b2c7c09ccbb0d191814eff17d8f71ffb7d0d2bc
mori_takumi <mori_takumi@plise.co.jp>
2023-11-15 17:36:17 +0900

    メモ追加機能の作成

--- src/main/java/com/example/demo/controller/MemoController.java
+++ src/main/java/com/example/demo/controller/MemoController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,11 +13,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.AccountDetails;
 import com.example.demo.entity.Memo;
 import com.example.demo.entity.User;
+import com.example.demo.model.form.MemoForm;
 import com.example.demo.repository.MemoRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
@@ -32,7 +36,9 @@ public class MemoController {
     @Autowired
     UserService userService;
 
-    /** メモ管理画面を表示します。 */
+    /**
+     * メモ管理画面を表示します。
+     */
     @GetMapping({ "/", "/{memoId}" })
     public String memo(
             @AuthenticationPrincipal AccountDetails accountDetails,
@@ -48,7 +54,28 @@ public class MemoController {
         model.addAttribute("memos", memos);
         model.addAttribute("memoDetail", memoDetail);
         model.addAttribute("profileForm", userService.createProfileForm(user));
+        model.addAttribute("memoForm", new MemoForm());
         return "memo/memo";
     }
 
+    /**
+     * メモの登録をします
+     */
+    @PostMapping("/create")
+    @Transactional
+    public String createMemo(
+            @AuthenticationPrincipal AccountDetails accountDetails,
+            MemoForm memoForm,
+            Model model) {
+        Memo memo = new Memo();
+        memo.setTitle(memoForm.getTitle());
+        memo.setContent(memoForm.getContent());
+        memo.setCreatedDate(LocalDate.now());
+        memo.setEditedDate(null);
+        memo.setUser(userRepository.findById(accountDetails.getId()).orElseThrow(EntityNotFoundException::new));
+        memo.setDeleteFlg(false);
+        memoRepository.save(memo);
+
+        return "redirect:/memo/";
+    }
 }
---
+++ src/main/java/com/example/demo/model/form/MemoForm.java
@@ -0,0 +1,26 @@
+package com.example.demo.model.form;
+
+public class MemoForm {
+
+    /** タイトル */
+    private String title;
+
+    /** コンテンツ */
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
